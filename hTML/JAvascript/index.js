33eb33640b8af1688413dec1090a76a23ff83a46
archit045 <architthakur90@gmail.com>
2023-12-13 18:25:09 +0530

    first commit

---
+++ hTML/JAvascript/index.js
@@ -0,0 +1,24 @@
+let a ="Archit"
+let b= 6
+console.log(a + b)
+
+
+console.log(typeof (a+b))
+
+ const c = {
+    name: "Harry",
+    section:1,
+    ispPrincipal: false
+ }
+ c['friend']="shubham"
+ a1['name']="Archit"
+ console.log(c)
+
+
+ const dict ={
+    appericiat:"recognize the full worth of",
+    Indolent:"Very Lazy",
+    Inteliggent:"Vey Smart",
+    Huge:"Very Large",
+    Massive:"Very Big"
+ }
